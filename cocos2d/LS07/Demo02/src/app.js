1146d29a6ac70989d2404c15dc10b2ffb471b57e
shichenxiao <1739118128@qq.com>
2018-03-25 20:09:22 +0800

    demo03

--- cocos2d/LS07/Demo02/src/app.js
+++ cocos2d/LS07/Demo02/src/app.js
@@ -2,34 +2,31 @@
 var HelloWorldLayer = cc.Layer.extend({
     sprite:null,
     ctor:function () {
-        //////////////////////////////
-        // 1. super init first
-        this._super();
 
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
+        this._super();
         var size = cc.winSize;
 
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = size.height / 2 + 200;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
+        var startMenuItem = new cc.MenuItemFont("开始",function(){
+            cc.log("开始按钮点击了！");
+        },this);
 
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2
-        });
-        this.addChild(this.sprite, 0);
+        var setMenuItem = new cc.MenuItemFont("设置",function(){
+            cc.log("设置按钮点击了！");
+        },this);
+        var ttfLabel = new cc.LabelTTF("其他","",30);
+        ttfLabel.setFontFillColor(cc.color.RED);
+        ttfLabel.enableStroke(cc.color.YELLOW,2);
+        ttfLabel.enableShadow(cc.color.WHITE,15,15,15);
+
+        var otherMenuItem = new cc.MenuItemLabel(ttfLabel,function(){
+            cc.log("其他按钮点击了！");
+        },this);
+
+        var menu = new cc.Menu(startMenuItem,setMenuItem,otherMenuItem);
+        menu.alignItemsHorizontally();
+
+        this.addChild(menu);
+        menu.y = size.height*0.3;
 
         return true;
     }
